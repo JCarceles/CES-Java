a77188401e0ba2a50881cfef782add2d9598df24
JCarceles <fco.javier.carceles.moreno@gmail.com>
2020-10-19 21:22:41 +0200

    Create Rectangulo.java

---
+++ src/poligonos/Rectangulo.java
@@ -0,0 +1,41 @@
+
+package poligonos;
+
+public class Rectangulo {
+    
+    private double lado1;
+    private double lado2;
+    
+    //public Rectangulo(){}
+    
+    public Rectangulo(double lado1, double lado2){
+        this.lado1=lado1;
+        this.lado2=lado2;    
+    }
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(double lado1) {
+        this.lado1 = lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+    
+    public double getArea(){
+        return lado1*lado2;
+    }
+
+    public double getPerimetro(){
+        return lado1*2+lado2*2;
+    }
+    
+    
+}
